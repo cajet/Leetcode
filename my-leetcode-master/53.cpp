f3e1c89e336aae1696a040f7dcd3e6dc9e1166b9
cajet <824160883@qq.com>
2017-09-27 10:44:19 +0800

    第四周

--- my-leetcode-master/53.cpp
+++ my-leetcode-master/53.cpp
@@ -1,3 +1,49 @@
+/* O(n^2)  */
+class Solution {
+public:
+	int maxSubArray(vector<int>& nums) {
+		int sum = 0;
+		int max = INT_MIN;
+		for (int i = 0; i < nums.size(); i++) {
+			sum = 0;
+			for (int j = i; j < nums.size(); j++) {
+				sum += nums[j];
+				if (sum > max) max = sum;
+			}
+		}
+		return max;
+	}
+};
+
+/*O(n*logn)*/
+
+int max(int a, int b, int c) {
+	if (a < b) a = b;
+	if (a > c) return a;
+	else return c;
+}
+
+int devide_conquer(vector<int>&  nums, int first, int end) {
+	if (first == end) return nums[first];
+	int mid = (first + end) / 2;
+	int maxLeft = INT_MIN, maxRight = INT_MIN, temp = 0;
+	for (int i = mid; i >= first; i--) {
+		temp += nums[i];
+		if (temp > maxLeft) maxLeft = temp;
+	}
+	temp = 0;
+	for (int j = mid + 1; j <= end; j++) {
+		temp += nums[j];
+		if (temp > maxRight) maxRight = temp;
+	}
+	return max(maxLeft + maxRight, devide_conquer(nums, first, mid), devide_conquer(nums, mid + 1, end));
+}
+
+int maxSubArray(vector<int>& nums) {
+	return devide_conquer(nums, 0, nums.size() - 1);
+}
+
+ /*O(n)*/
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -16,3 +62,5 @@ public:
       return max;
     }
 };
+
+ 
